a233a9519a438fcf05767ff9c7906f941cdbf60d
Juan Tabares <juan.ctt@live.com>
2013-07-08 15:04:20 +0900

    modifying grader

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var CHECKURL_DEFAULT = "http:\\www.google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +37,11 @@ var assertFileExists = function(infile) {
 	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertUrlExists = function(urlfile) {
+    var urlstr = urlfile.toString();
+    return urlstr;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,6 +73,7 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <urk_point>','Url to web page', clone(assertUrlExists), HTMLFILE_DEFAULT)
 	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
