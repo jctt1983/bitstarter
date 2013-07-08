346fc3a86ed78491371f80737ecbe46f1d5daa57
Juan Tabares <juan.ctt@live.com>
2013-07-08 11:22:20 +0900

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = requre('fs');
+
+var getStringFromIndexFile = function(filename){
+    var buffer = fs.readFileSync(filename);
+    return buffer.toString();
+};
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var message = getStringFromIndexFile('index.html');
+    response.send(message);
 });
 
 var port = process.env.PORT || 5000;
