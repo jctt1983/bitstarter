0305c6915ac57b6c80e12171eb64d8ecbcf29017
Juan Tabares <juan.ctt@live.com>
2013-07-22 02:27:46 +0900

    changing layout and presentation

--- web.js
+++ web.js
@@ -13,6 +13,17 @@ app.get('/', function(request, response) {
     response.send(message);
 });
 
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({
+    dumpExceptions: true, 
+    showStack: true
+  }));
+  app.use(app.router);
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
